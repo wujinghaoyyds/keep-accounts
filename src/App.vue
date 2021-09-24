699f7eaf070e43784753f35375a131740df96e79
wujinghao <wujinghaowork@foxmail.com>
2021-09-24 20:45:28 +0800

    Money 页面布局

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@
 </template>
 
 <style lang="scss">
+*{
+  margin:0;padding:0;
+  box-sizing:border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -12,14 +16,11 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
-
     &.router-link-exact-active {
       color: #42b983;
     }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    Money
+  <div class="nav-wrapper">
+    <div class="content">
+      Money
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +14,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+  border:1px solid green;
+  display:flex;
+  flex-direction:column;
+  height:100vh;
+}
+.content{
+  border:10px solid blue;
+  overflow:auto;
+  flex-grow:1;
+}
 </style>
