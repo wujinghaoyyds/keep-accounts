70a8bee7ee7f872ccdfb93e0ef4555a190016f0b
wujinghao <wujinghaowork@foxmail.com>
2021-09-25 16:06:32 +0800

    添加全局变量和样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -22,6 +22,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '~@/assets/style/helper.scss';
 nav {
   display: flex;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -39,8 +40,8 @@ nav {
       height: 32px;
     }
   }
-  > .item.selected{
-    color: blue;
+  > .item.selected {
+    color: $color-highlight;
   }
 }
 
