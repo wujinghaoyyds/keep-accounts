e26941d809b0688e5cff87b6e0870be2bf7ac961
wujinghao <wujinghaowork@foxmail.com>
2021-09-25 13:13:42 +0800

    添加页面选中高亮

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav class="nav">
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="labels"/>
       标签
     </router-link>
-    <router-link to="statistics" class="item">
+    <router-link to="statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -40,7 +40,7 @@ nav {
     }
   }
   > .item.selected{
-    color: red;
+    color: blue;
   }
 }
 
