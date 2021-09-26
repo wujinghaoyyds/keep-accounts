f2e5ca2493a47c05b834ba1480536576b8e618ce
wujinghao <wujinghaowork@foxmail.com>
2021-09-26 21:22:04 +0800

    添加时间createAt和数据迁移

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -4,7 +4,6 @@
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
-    {{ record }}
   </Layout>
 </template>
 
@@ -15,20 +14,32 @@ import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component, Watch} from 'vue-property-decorator';
+const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+
+// const version = window.localStorage.getItem('version') || '0';
+// if (version === '0.0.1') {
+//   // 数据库升级。数据迁移
+//   recordList.forEach(record => {
+//     record.createAt = new Date(2021, 9, 26);
+//   });
+//   //  保存数据
+//   window.localStorage.setItem('recordList', JSON.stringify(recordList));
+// }
+// window.localStorage.setItem('version', '0.0.1');
 
 type Record = {
   tags: string[]
   notes: string
   type: string
   amount: number
+  createAt?: Date
 }
-
 @Component({
   components: {Tags, Notes, Types, NumberPad}
 })
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行'];
-  recordList: Record[] = [];
+  recordList: Record[] = recordList;
   record: Record = {tags: [], notes: '', type: '-', amount: 0};
 
   onUpdateTags(value: string[]) {
@@ -40,9 +51,9 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    const record2 = JSON.parse(JSON.stringify(this.record));//  深拷贝
+    const record2: Record = JSON.parse(JSON.stringify(this.record));//  深拷贝
+    record2.createAt = new Date();
     this.recordList.push(record2);
-    console.log(this.recordList);
   }
 
   @Watch('recordList')
