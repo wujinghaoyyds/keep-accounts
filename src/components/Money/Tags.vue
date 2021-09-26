8b0a8eb9d91ffe98b16e744cd7ac51136519465a
wujinghao <wujinghaowork@foxmail.com>
2021-09-26 15:59:46 +0800

    可以新建tag

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
       <li v-for="tag in dataSource" :key="tag" :class="{selected:selectedTags.indexOf(tag)>=0}" @click="toggle(tag)">
@@ -17,7 +17,7 @@ import {Component, Prop} from 'vue-property-decorator';
 
 @Component
 export default class Tags extends Vue {
-  @Prop() dataSource: string[] | undefined;
+  @Prop() readonly dataSource: string[] | undefined;
   selectedTags: string[] = [];
 
   toggle(tag: string) {
@@ -27,7 +27,15 @@ export default class Tags extends Vue {
     } else {
       this.selectedTags.push(tag);
     }
+  }
 
+  create() {
+    const name = window.prompt('请输入标签名');
+    if (name === '') {
+      window.alert('标签名不能为空');
+    } else if (this.dataSource) {
+        this.$emit('update:dataSource', [...this.dataSource, name]);
+      }
   }
 }
 </script>
