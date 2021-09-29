8d8eda54a772991720d1ec684ab33962baff2bbc
wujinghao <wujinghaowork@foxmail.com>
2021-09-29 23:03:08 +0800

    模块化的store，删除了model 发现一个bug 在money页面无法新建标签

---
+++ src/store/recordStore.ts
@@ -0,0 +1,22 @@
+import clone from '@/lib/clone';
+
+const localStorageKeyName = 'recordList';
+
+const recordStore = {
+    recordList: [] as RecordItem[],
+    fetch() {
+        this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+        return this.recordList;
+    },
+    saveRecords() {
+        window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.recordList));
+    },
+    createRecord(record: RecordItem) {
+        const record2: RecordItem = clone(record);//  深拷贝
+        record2.createdAt = new Date();
+        this.recordList?.push(record2);//可选链语法
+        recordStore.saveRecords();
+    }
+};
+recordStore.fetch();
+export default recordStore;
